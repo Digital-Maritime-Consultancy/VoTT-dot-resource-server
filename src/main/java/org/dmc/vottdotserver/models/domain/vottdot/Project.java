adf2ca4bee18850b1bfa0b70bd153abc9a35f855
Jinki Jung <your.jinki.jung@gmail.com>
2022-08-08 10:11:21 +0200

    having task ID as an attribute

--- src/main/java/org/dmc/vottdotserver/models/domain/vottdot/Project.java
+++ src/main/java/org/dmc/vottdotserver/models/domain/vottdot/Project.java
@@ -23,6 +23,7 @@ public class Project implements Serializable {
     private String description;
     private VideoSetting videoSettings;
     private DotToRectSetting dotToRectSettings;
+    private String taskId;
     private String taskType;
     private String taskStatus;
     private List<Tag> tags;
@@ -81,6 +82,7 @@ public class Project implements Serializable {
         dotToRectSettings.put("url", task.getDotToRectUrl());
         obj.put("dotToRectSettings", dotToRectSettings);
 
+        obj.put("taskId", task.getId());
         obj.put("taskType", task.getType());
         obj.put("taskStatus", task.getStatus());
 
